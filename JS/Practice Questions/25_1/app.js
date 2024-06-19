039117d2c683eb59947ac222746c92c369e56319
harshada <samaneharshada04@gmail.com>
2024-06-19 22:06:48 +0530

    Practice questions
    Practice questions of function

---
+++ JS/Practice Questions/25_1/app.js
@@ -0,0 +1,15 @@
+function printPoem() {
+    console.log("Twinkle Twinkle, little Star");
+    console.log("how I wonder what you are");
+}
+
+printPoem();
+
+
+
+function rollDice() {
+    let randomNum = Math.floor(Math.random() * 6) + 1;
+    console.log(randomNum);
+}
+
+rollDice();
