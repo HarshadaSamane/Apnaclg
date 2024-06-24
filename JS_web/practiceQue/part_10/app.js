c767130a6a00ac34acd4cf3818988bfc564a963b
harshada <samaneharshada04@gmail.com>
2024-06-24 22:37:43 +0530

    Completed part 10
    Completed all the Practice questions and  activity.

---
+++ JS_web/practiceQue/part_10/app.js
@@ -0,0 +1,17 @@
+// let btn = document.querySelector("button");
+// console.dir(btn);
+
+// btn.onclick = function () {
+//     console.log("button was clicked");
+// };
+
+// btn.onmouseenter = function () {
+//     console.log("You entered a button");
+// };
+
+
+let inp = document.querySelector("input");
+
+inp.addEventListener("keydown", function() {
+    console.log("Key was pressed");
+});
