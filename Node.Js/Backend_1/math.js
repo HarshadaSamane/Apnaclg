7086346f3a43a2be8481e971da1397220a3d874f
harshada <samaneharshada04@gmail.com>
2024-06-28 22:23:54 +0530

    Started node.js
    Completed the first part of backend i.e node

---
+++ Node.Js/Backend_1/math.js
@@ -0,0 +1,25 @@
+// module.exports.div = (a,b) => a / b;
+
+// const sum  = (a, b) => a + b;
+// const sub = (a, b) => a - b;
+// const mul = (a, b) => a * b;
+
+// const g = 9.8;
+// const PI = 3.14;
+
+// let obj = {
+//     sum : sum,
+//     sub : sub,
+//     mul : mul,
+//     g : g,
+//     PI : PI
+// }
+
+// module.exports = obj;
+
+
+export const summation = (a,b) => a + b;
+export const multiplication = (a,b) => a * b;
+
+export const PI1 = 3.14;
+export const G = 9.8;
---
+++ Node.Js/Backend_1/script.js
@@ -0,0 +1,14 @@
+// // const math = require("./math");
+
+// // // console.log(math.mul(2,7));
+// // console.log(math);
+
+// const info = require("../fruits");
+// console.log(info);
+
+
+import { summation, PI1 } from "./math.js";
+console.log(summation(2,5));
+
+import { generate } from "random-words";
+console.log(generate());
