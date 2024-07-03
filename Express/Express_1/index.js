91e7dde134472977e8b70807a07e6b9a0f129f17
harshada <samaneharshada04@gmail.com>
2024-07-03 23:19:04 +0530

    updated index.js
    Updated the index.js. Learned more concepts.

--- Express/Express_1/index.js
+++ Express/Express_1/index.js
@@ -4,9 +4,46 @@ const app = express();
 let port = 3000;
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
 
-app.use((req, res) => {
-    console.log("Request received");
+app.get("/", (req, res) => {
+  res.send("This is a root path");
 });
+
+// QUERY STRING
+app.get("/search", (req, res) => {
+  console.log(req.query);
+  res.send("no results");
+});
+
+// PATH PARAMETERS
+app.get("/:username", (req, res) => {
+  // console.log(req.params);
+  // console.log("Hello, I am root");
+  let { username } = req.params;
+  let htmlStr = `<h1>Welcome to the page, @${username}</h1>`;
+  res.send(htmlStr);
+  console.log(htmlStr);
+});
+
+// app.get("/apple", ( req, res ) => {
+//     res.send("Contacted to apple path");
+// });
+
+// app.get("*", (req, res) =>{
+//     res.send("This path does not exist");
+// });
+
+// app.post("/", (req,res) => {
+//     res.send("You sent a post request to root");
+// });
+
+// app.use((req, res) => {
+//     // console.log(req);
+//     console.log("Request received");
+//     res.send( {
+//         name: "Apple",
+//         color: "Red",
+//     });
+// });
