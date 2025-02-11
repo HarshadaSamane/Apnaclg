75d090a87897167288c355806ecf20f67cd6a3de
harshada <samaneharshada04@gmail.com>
2025-02-11 22:39:28 +0530

    Solved rotated and sorted array problem

--- DSA/Questions/DividenCon_23/rsorted.java
+++ DSA/Questions/DividenCon_23/rsorted.java
@@ -2,7 +2,7 @@ package Questions.DividenCon_23;
 
 public class rsorted {
 
-    public static int search(int arr, int tar, int si, int ei) {
+    public static int search(int arr[], int tar, int si, int ei) {
         if(si > ei) {
             return -1;
         }
@@ -12,8 +12,36 @@ public class rsorted {
         if(arr[mid] == tar) {
             return mid;
         }
+
+        //mid on L1
+        if(arr[si] < arr[mid]) {
+            //case a: left part
+            if(arr[si] <= tar && tar <= arr[mid]) {
+                return search(arr, tar, si, mid-1);
+            }
+            else {
+                //case b: right part
+                return search(arr, tar, mid+1, ei);
+            }
+        }
+
+        //mid on L2
+        else {
+            //case c: right part
+            if(arr[mid] <= tar && tar <= arr[ei]) {
+                return search(arr, tar, mid+1, ei);
+            }
+            else {
+                //case d: left
+                return search(arr, tar, si, mid-1);
+            }
+        }
     }
     public static void main(String[] args) {
-        int arr = {}
+        int arr[] = {4, 5, 6, 7, 0, 1, 2};
+        int tar = 0;
+        int tarIdx = search(arr, tar, 0, arr.length-1);
+        System.out.println(tarIdx);
+
     }
 }
