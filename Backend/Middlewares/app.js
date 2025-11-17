7c9ca19b53c744577980f94beb363dc455140482
harshada <samaneharshada04@gmail.com>
2025-11-17 16:30:46 +0530

    Error Handling
    Learned how to handle different types of Errors

--- Backend/Middlewares/app.js
+++ Backend/Middlewares/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const ExpressError = require("./ExpressError");
 
 // app.use((req, res, next) => {
 //   //   let { query } = req.query;
@@ -14,7 +15,7 @@ const checkToken = (req, res, next) => {
   if (token === "giveaccess") {
     next();
   }
-  res.send("ACCESS DENIED!");
+  throw new ExpressError(401, "ACCESS DENIED!");
 };
 
 app.get("/api", checkToken, (req, res) => {
@@ -35,6 +36,20 @@ app.get("/random", (req, res) => {
 //   console.log(req.method, req.hostname, req.path, req.time);
 //   next();
 // });
+
+app.get("/err", (req, res) => {
+  abcd = abcd;
+});
+
+app.get("/admin", (req, res) => {
+  throw new ExpressError(403, "Access to admin is forbidden");
+});
+
+//ERROR
+app.use((err, req, res, next) => {
+  let { status = 500, message = "Some Error Occured" } = err;
+  res.status(status).send(message);
+});
 
 //404
 app.use((req, res) => {
